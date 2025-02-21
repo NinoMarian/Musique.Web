e25b1049c2d674b83be021b87cb2b257c768cdde
NinoMarian <ninom0801@gmail.com>
2025-02-21 16:06:44 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,65 @@
+/*var musiques =[
+    "Here With Me by d4vd", 
+    "Mmhmm by BigXthatPlug", 
+    "The Feels by Twice", 
+    "Ransom by Lil Tecca", 
+    "Revenge by XXXTentation"
+];
+
+// musiques.forEach(function(e) {
+//     console.log("Musique : " + e);
+// })
+
+
+var descriptionMusiques = [
+    "Une de mes musiques préféré.",
+    "Pour me donner un p'tit coup de boost.",
+    "Je la connais par <3",
+    "10/10 ça me rapelle l'époque du collège.",
+    "musique nocturne."
+]
+
+// descriptionMusiques.forEach(function(e){
+//     console.log("Description : " + e);
+// })
+*/
+
+
+const data = [
+    {
+        titre : "Here With Me by d4vd",
+        descriptionMusicale: "Une de mes musiques préféré.",
+        urlson : "Musique/d4vd.mp3",
+        image : "image/d4vd.jpg",
+        lien : "https://www.youtube.com/watch?v=Ip6cw8gfHHI&ab_channel=d4vdVEVO",
+     },
+    
+    {
+        titre : "Mmhmm by BigXthatPlug",
+        descriptionMusicale: "Pour me donner un p'tit coup de boost.",
+        urlson : "Musique/mhm.mp3",
+        image : "image/bigxthatplug.jpg",
+        lien : "https://www.youtube.com/watch?v=x_WayannFys&ab_channel=BigXthaPlug",
+    },
+    {
+        titre : "The Feels by Twice",
+        descriptionMusicale: "Je la connais par <3",
+        urlson : "Musique/twice.mp3",
+        image : "image/twice.jpg",
+        lien : "https://www.youtube.com/watch?v=f5_wn8mexmM&ab_channel=JYPEntertainment",
+    },
+    {
+        titre : "Ransom by Lil Tecca",
+        descriptionMusicale: "10/10 ça me rapelle l'époque du collège.",
+        urlson : "Musique/rsm.mp3",
+        image : "image/lil-tecca.jpg",
+        lien : "https://www.youtube.com/watch?v=1XzY2ij_vL4&ab_channel=LyricalLemonade",
+    },
+    {
+        titre : "Revenge by XXXTentation",
+        descriptionMusicale: "Musique nocturne",
+        urlson : "Musique/revenge.mp3",
+        image : "image/xxxtentation.jpg",
+        lien : "https://www.youtube.com/watch?v=CD_tD26E7k0&ab_channel=XXXTENTACION",
+    }
+];
